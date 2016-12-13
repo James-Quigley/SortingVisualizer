89bb9ce133c59e12331271b3936b3a9b0fb7555a
James Quigley <james@quigley.us>
2016-12-12 21:07:40 -0700

    Added insertion and merge. Quick sort not yet functional

--- sorting.js
+++ sorting.js
@@ -33,3 +33,80 @@ function selectionSort(data) {
     }
     return data;
 }
+
+function mergeSort(data) {
+    if (data.length < 2) {
+        return data;
+    }
+    var mid = parseInt(data.length/2);
+    var left = data.slice(0,mid);
+    var right = data.slice(mid, data.length);
+    
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    var result = [];
+    
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        }
+        else {
+            result.push(right.shift());
+        }
+    }
+    
+    while(left.length) {
+        result.push(left.shift());
+    }
+    
+    while(right.length) {
+        result.push(right.shift());
+    }
+    
+    return result;
+}
+
+function insertionSort(data) {
+    for (var i = 1; i < data.length; i++) {
+        var x = data[i];
+        var j = i - 1;
+        while (j >= 0 && data[j] > x) {
+            data[j+1] = data[j];
+            j = j - 1;
+        }
+        data[j+1] = x;
+    }
+    return data;
+}
+
+function quickSort(data, left, right) {
+    var len = data.length, pivot, partitionIndex;
+    
+    if (left < right) {
+        pivot = right;
+        partitionIndex = partition(data, pivot, left, right);
+        quickSort(data, left, partitionIndex - 1);
+        quickSort(data, partitionIndex + 1, right);
+    }
+    
+    return data;
+}
+
+function partition(data, pivot, left, right) {
+    var pivotValue = data[pivot], partitionIndex = left;
+    
+    for (var i = left; i < right; i++) {
+        if (data[i] < pivotValue) {
+            var tmp = data[i];
+            data[i] = data[partitionIndex];
+            data[partitionIndex] = tmp;
+            partitionIndex++;
+        }
+    }
+    var tmp = data[right];
+    data[right] = data[partitionIndex];
+    data[partitionIndex] = tmp;
+    return partitionIndex;
+}
